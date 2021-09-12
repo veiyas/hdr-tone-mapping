24699b25c3ae30b239adccb5952281899b161eb7
Algot Sandahl <algot.sandahl@gmail.com>
2021-09-12 19:13:20 +0200

    Add matlab function from paper and image sequence loading

---
+++ src/getImageSequence.m
@@ -0,0 +1,17 @@
+function [pixelValues, exposureTimes] = getImageSequence(sequenceId)
+
+dataDirectory = "../data/";
+
+if strcmp(sequenceId, "JPEGS_FROM_LISAM")
+    ids = 5165:5175;
+    pixelValues = {};
+    exposureTimes = {};
+    for i = 1:length(ids)
+        name = strcat(dataDirectory, "/HDR/IMG_", num2str(ids(i)), ".JPG");
+        pixelValues{i} = imread(name);
+        exposureTimes{i} = imfinfo(name).DigitalCamera.ExposureTime;
+    end
+end 
+
+end
+
--- src/main.m
+++ src/main.m
@@ -1,2 +1,8 @@
-disp("hej svejs världen");
-% imshow(imread("../data/HDR/Img10.tiff"));
+%% Clear
+clear
+format compact
+
+%% Get images
+[pixelValues, exposureTimes] = getImageSequence("JPEGS_FROM_LISAM");
+disp(strcat("Number of images: ", num2str(length(pixelValues))));
+montage(pixelValues);
