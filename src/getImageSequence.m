86cb7fdf937802b7e2c5a6d98dbccbc52d6691dc
Algot Sandahl <algot.sandahl@gmail.com>
2021-09-12 19:19:34 +0200

    Fix mistake when loading images

--- src/getImageSequence.m
+++ src/getImageSequence.m
@@ -3,7 +3,7 @@ function [pixelValues, exposureTimes] = getImageSequence(sequenceId)
 dataDirectory = "../data/";
 
 if strcmp(sequenceId, "JPEGS_FROM_LISAM")
-    ids = 5165:5175;
+    ids = 5165:5178;
     pixelValues = {};
     exposureTimes = {};
     for i = 1:length(ids)
